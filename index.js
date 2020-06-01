a6c448440bd0242717aecf10c9c15de98a92538c
anjingwsno1 <46479333+anjingwsno1@users.noreply.github.com>
2020-06-01 15:51:05 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
